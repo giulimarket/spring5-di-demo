204394d3ba27a855e2187a17d08935bc3210bbf8
giulimarket <giuli.market@gmail.com>
2019-03-22 17:04:14 +0100

    Lezione 3-42

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"en", "default"})
+@Profile("de")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
-		return "Hello - Primary Greeting service";
+		return "Primarer Grubdienst";
 	}
 
 }
